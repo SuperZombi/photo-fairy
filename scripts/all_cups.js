25f154e2762ca2918627401e01df872fca50ec99
Super Zombi <rost201541@gmail.com>
2021-09-02 12:06:20 +0300

    Update all_cups.js

--- scripts/all_cups.js
+++ scripts/all_cups.js
@@ -198,6 +198,11 @@ function check_groups(clicked){
 window.onload = function(){
 	try{
 		document.getElementById("logo").innerHTML = logo_;
+
+		document.getElementById("body").innerHTML += '<div style="width:100%;float:right;height:50px;"></div>'
+		ul = document.createElement("ul");
+		ul.innerHTML = social_networks_;
+		document.getElementById("body").appendChild(ul);
 	}
 	catch{;}
 
